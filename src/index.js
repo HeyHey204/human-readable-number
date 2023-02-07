9613dc6e9452a303d1085e0c2d4165b494ffebfd
Alexey Gerasimov <alexger204@gmail.com>
2023-02-07 16:08:00 +0500

    init: started and finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+	let ones, tens, hundreds = 0;
+	let onesStringsArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+	let tensStringsArray = ['', [
+		'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+	], 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+	let hundredsStringsArray = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+
+	let numStringReversed = number.toString().split('').reverse();
+	console.log(numStringReversed);
+
+	[ones, tens, hundreds] = numStringReversed;
+
+	let hundredsString = '';
+	let tensOnesString = '';
+	let tensString = '';
+	let onesString = '';
+
+	if (number == 0)
+		return 'zero';
+	if (number < 10)
+		return onesStringsArray[ones];
+	if (hundreds >= 0)
+		hundredsString = hundredsStringsArray[hundreds];
+	if (+(tens + ones) > 9 && +(tens + ones) < 20) {
+		tensOnesString = tensStringsArray[tens][ones];
+		return [hundredsString, tensOnesString].join(' ').replace(/\s+/g, ' ').trim();
+	}
+	if (tens >= 0)
+		tensString = tensStringsArray[tens];
+	if (ones >= 0)
+		onesString = onesStringsArray[ones];
+
+	return [hundredsString, tensString, onesString].join(' ').replace(/\s+/g, ' ').trim();
 }
